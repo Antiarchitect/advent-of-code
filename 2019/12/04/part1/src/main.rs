8bf9eeecd3c47ebd1d5ab59d49335b17079e1b52
Andrey Voronkov <voronkovaa@gmail.com>
2019-12-14 15:48:21 +0300

    Better formatting

--- 2019/12/04/part1/src/main.rs
+++ 2019/12/04/part1/src/main.rs
@@ -2,19 +2,23 @@ fn main() {
     let lower: u64 = 382345;
     let upper: u64 = 843167;
 
-    let count = (lower..=upper).filter(|&x| {
-        let mut current = 0;
-        let mut has_successives = false;
-        for c in x.to_string().chars() {
-            let digit = c.to_digit(10).expect("Cannot parse digit");
-            if digit < current {
-                return false;
-            } else {
-                if current == digit { has_successives = true; }
-                current = digit;
+    let count = (lower..=upper)
+        .filter(|&x| {
+            let mut current = 0;
+            let mut has_successives = false;
+            for c in x.to_string().chars() {
+                let digit = c.to_digit(10).expect("Cannot parse digit");
+                if digit < current {
+                    return false;
+                } else {
+                    if current == digit {
+                        has_successives = true;
+                    }
+                    current = digit;
+                }
             }
-        }
-        has_successives
-    }).count();
+            has_successives
+        })
+        .count();
     println!("{}", count);
 }
