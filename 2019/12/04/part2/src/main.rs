acbe36f2e8a4a639f0ddcedff601554a2c97a5cd
Andrey Voronkov <voronkovaa@gmail.com>
2019-12-14 19:32:21 +0300

    Still having wrong answer

--- 2019/12/04/part2/src/main.rs
+++ 2019/12/04/part2/src/main.rs
@@ -8,10 +8,14 @@ fn main() {
             for c in x.to_string().chars() {
                 let digit = c.to_digit(10).expect("Cannot parse digit");
                 match clustered.last_mut() {
-                    None => { clustered.push(vec![digit]); },
+                    None => {
+                        clustered.push(vec![digit]);
+                    }
                     Some(last) => {
                         let last_digit = *last.last().unwrap();
-                        if last_digit > digit { return false; };
+                        if last_digit > digit {
+                            return false;
+                        };
                         if last_digit == digit {
                             last.push(digit);
                         } else {
@@ -22,14 +26,16 @@ fn main() {
             }
 
             // Remove combinations having no doubles at all
-            if clustered.len() == 6 { return false };
+            if clustered.len() == 6 {
+                return false;
+            };
 
             // Check clustered array has only proper possible combinations (even doubles and single digits)
-            let proper = clustered.iter().filter(|&x| x.len() == 1 || x.len() % 2 == 0);
-            if proper.clone().count() != clustered.len() {
+            let proper = clustered.iter().all(|x| x.len() == 1 || x.len() % 2 == 0);
+            if !proper {
                 return false;
             }
-            println!("{:?}", proper);
+            println!("{:?}", clustered);
             true
         })
         .count();
