0b423d887e9d03990ffddd87dc59e4f08abd617e
Andrey Voronkov <voronkovaa@gmail.com>
2019-12-15 16:46:18 +0300

    Complete part2 of 2019/12/04

--- 2019/12/04/part2/src/main.rs
+++ 2019/12/04/part2/src/main.rs
@@ -1,43 +1,80 @@
-fn main() {
-    let lower: u32 = 382345;
-    let upper: u32 = 843167;
+struct Combination {
+    current: String,
+    last: String,
+}
 
-    let count = (lower..=upper)
-        .filter(|&x| {
-            let mut clustered: Vec<Vec<_>> = Vec::new();
-            for c in x.to_string().chars() {
-                let digit = c.to_digit(10).expect("Cannot parse digit");
-                match clustered.last_mut() {
-                    None => {
+impl Iterator for Combination {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        if self.current == self.last {
+            return None;
+        };
+
+        let mut memory = 0;
+
+        self.current = self
+            .current
+            .chars()
+            .rev()
+            .fold(Vec::new(), |mut acc, c| {
+                let mut x = c.to_digit(10).expect("Cannot parse digit");
+
+                x += memory;
+
+                match acc.last() {
+                    None => x += 1,
+                    Some(_) => {}
+                }
+
+                if x == 10 {
+                    memory = 1;
+                    x = 0;
+                } else {
+                    memory = 0;
+                }
+
+                acc.push(std::char::from_digit(x, 10).expect("Cannot construct digit"));
+                acc
+            })
+            .iter()
+            .rev()
+            .collect::<String>();
+
+        Some(self.current.to_owned())
+    }
+}
+
+fn main() {
+    let count = Combination {
+        current: "382345".to_string(),
+        last: "843167".to_string(),
+    }
+    .filter(|x| {
+        let mut clustered = Vec::new();
+        for c in x.to_string().chars() {
+            let digit = c.to_digit(10).expect("Cannot parse digit");
+            match clustered.last_mut() {
+                None => {
+                    clustered.push(vec![digit]);
+                }
+                Some(last) => {
+                    let last_digit = *last.last().unwrap();
+                    if last_digit > digit {
+                        return false;
+                    };
+                    if last_digit == digit {
+                        last.push(digit);
+                    } else {
                         clustered.push(vec![digit]);
-                    }
-                    Some(last) => {
-                        let last_digit = *last.last().unwrap();
-                        if last_digit > digit {
-                            return false;
-                        };
-                        if last_digit == digit {
-                            last.push(digit);
-                        } else {
-                            clustered.push(vec![digit]);
-                        }
                     }
                 }
             }
+        }
 
-            // Remove combinations having no doubles at all
-            if clustered.len() == 6 {
-                return false;
-            };
-
-            // Check clustered array has only proper possible combinations (even doubles and single digits)
-            let proper = clustered.iter().all(|x| x.len() == 1 || x.len() % 2 == 0);
-            if !proper {
-                return false;
-            }
-            println!("{:?}", clustered);
-            true
-        })
-        .count();
+        // Check clustered array has at least one cluster with exaclty 2 numbers in it
+        clustered.iter().any(|x| x.len() == 2)
+    })
+    .count();
     println!("{}", count);
 }
