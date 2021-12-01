934c657c3037f48a6c39a6b54b77f1d7e42b0989
Andrey Voronkov <voronkovaa@gmail.com>
2021-12-01 18:28:39 +0300

    Cargo fmt

--- 2021/01/part1/src/main.rs
+++ 2021/01/part1/src/main.rs
@@ -6,19 +6,18 @@ use std::io::BufReader;
 fn main() {
     let file = File::open("sonar.txt").expect("Cannot open file");
 
-    let result: (usize, Option<u64>) = BufReader::new(file)
-        .lines()
-        .fold((0, None), |acc, line| {
-            let current = line.expect("Cannot read line").parse::<u64>()
-                .expect("Cannot parse positive integer");
-            if let Some(previous) = acc.1 {
-                if current > previous {
-                    return (acc.0 + 1, Some(current));
-                }
-
-            };
-            (acc.0, Some(current))
-        });
+    let result: (usize, Option<u64>) = BufReader::new(file).lines().fold((0, None), |acc, line| {
+        let current = line
+            .expect("Cannot read line")
+            .parse::<u64>()
+            .expect("Cannot parse positive integer");
+        if let Some(previous) = acc.1 {
+            if current > previous {
+                return (acc.0 + 1, Some(current));
+            }
+        };
+        (acc.0, Some(current))
+    });
 
     println!("{}", result.0);
 }
